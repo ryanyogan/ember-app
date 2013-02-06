8b30b9d17f03af0fc3f98d11df629f366446c8ce
Ryan Yogan <ryanyogan@gmail.com>
2013-02-06 14:44:33 -0600

    DB SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
-User.create(first_name: 'William', last_name: 'Harrison', quote: 'I like to kick rocks')
-User.create(first_name: 'Ryan', last_name: 'Yogan', quote: 'Rock lobster is what is on')
+User.create(first_name: 'Will',
+            last_name:  'Ferril',
+            quote: 'I am going to fill a pillow case with oranges and beat the shit out of you.')
+User.create(first_name: 'John C.',
+            last_name: 'Reilly',
+            quote: 'My name is Dale but you have to call me Dragon')
+User.create(first_name: 'Tom',
+            last_name: 'Cruise',
+            quote: 'I am a fucking lunatic, but Top Gun was legit.')
+User.create(first_name: 'Ben',
+            last_name: 'Franklin',
+            quote: 'I seceretly loved little boys, no big deal...')
+
